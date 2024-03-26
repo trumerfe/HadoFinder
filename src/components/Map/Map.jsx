ed4a62e21aba1e3e8f8af604e6bc8ee9657ba40e
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-03-26 11:15:07 -0400

    added geolocation support to the map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,8 +7,8 @@ const Map = (props) => {
 
   const mapRef = useRef(null);
 
-  const [latitude, setLatitude] = useState(40.72515026722599)
-  const [longitude, setLongitude] = useState(-73.99676899560035)
+  const [latitude, setLatitude] = useState(40)
+  const [longitude, setLongitude] = useState(-73)
 
   console.log(latitude)
   console.log(longitude)
@@ -24,7 +24,7 @@ const Map = (props) => {
   return (
     // Component from the leaflet react library
     <MapContainer
-      center={[latitude, longitude]}
+      center={[40.72515026722599, -73.99676899560035]}
       zoom={13}
       ref={mapRef}
       // changes the dimensions of the map component
@@ -37,11 +37,19 @@ const Map = (props) => {
       {/* Sets position of marker (to be populated by coordinates provided by geolocation and API calls)
           Currently, state goes undefined for a moment when fetching geolocation data which breaks the page.
           Must set a placeholder to fix. */}
-      <Marker position={[40.72515026722599, -73.99676899560035]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+          {latitude === undefined ? 
+            (<Marker opacity={0} position={[40.72515026722599, -73.99676899560035]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker> )
+            : 
+            (<Marker opacity={1.0} position={[latitude, longitude]}>
+              <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>)
+          }
     </MapContainer>
   );
 };
