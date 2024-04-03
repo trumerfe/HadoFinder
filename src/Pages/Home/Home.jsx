5bfe525ea71c9c5e9b2a5cd78e61a24fe19a5f72
Zoe <66967044+trumerfe@users.noreply.github.com>
2024-04-03 16:58:07 -0400

    worked on graphQL testing for the start.gg api

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,35 +1,113 @@
-import './Home.scss'
-import MonthPicker from "../../components/MonthPicker/MonthPicker"
-import Map from "../../components/Map/Map"
-import { useState, useEffect } from 'react'
+import "./Home.scss";
+import MonthPicker from "../../components/MonthPicker/MonthPicker";
+import Map from "../../components/Map/Map";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { end } from "@popperjs/core";
 
-const apiKey = "c817c22c23246bc906960a32bf1fc5c8"
+const apiKey = "c817c22c23246bc906960a32bf1fc5c8";
+
+const headers = {
+  "Authorization": "Bearer c817c22c23246bc906960a32bf1fc5c8",
+};
+
+const endpoint = "https://api.start.gg/gql/alpha";
+
+const query = {
+  operationName: "getTournaments",
+  query: `query Sets {
+    player(id: 1000) {
+      id
+      sets(perPage: 5, page: 10) {
+        nodes {
+          id
+          displayScore
+          event {
+            id
+            name
+            tournament {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }`,
+  // variables: {
+  //   perPage: 4,
+  //   coordinates: "33.7454725,-117.86765300000002",
+  //   radius: "50mi",
+  // },
+};
 
 const Home = () => {
+  const graphqlTest = async () => {
+    try {
+      const response = await axios({
+        url: endpoint,
+        method: "post",
+        headers: headers,
+        data: {
+          query: `
+          query SocalTournaments($perPage: Int, $coordinates: String!, $radius: String!) {
+            tournaments(query: {
+              perPage: $perPage
+              filter: {
+                location: {
+                  distanceFrom: $coordinates,
+                  distance: $radius
+                }
+              }
+            }) {
+              nodes {
+                name
+                city
+                venueAddress
+                startAt
+              }
+            }
+          }
+          `,
+          variables: {
+            "perPage": 1,
+            "coordinates": "40.72515026722599, -73.99676899560035",
+            "radius": "5mi"
+          }
+        },
+      });
+      console.log(response.data.data.tournaments.nodes[0]);
+      console.log(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const [location, setLocation] = useState([])
+  graphqlTest();
+
+  const [location, setLocation] = useState([]);
 
   function error() {
-    console.log('Unable to retrieve your location')
+    console.log("Unable to retrieve your location");
   }
 
   function success(position) {
-    const lat = position.coords.latitude
-    const lon = position.coords.longitude
-    const location = [lat, lon]
-    setLocation(location)
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const location = [lat, lon];
+    setLocation(location);
   }
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success, error)
-  },[])
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
 
   return (
     <main>
       <MonthPicker />
       <Map location={location} />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
