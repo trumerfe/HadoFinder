214b25e7d588bdce75fd111a50a21eb69e53978f
Ruby <erictrumer@gmail.com>
2024-04-03 22:07:40 -0400

    did some changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,42 +5,31 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const apiKey = "c817c22c23246bc906960a32bf1fc5c8";
-
 const headers = {
   "Authorization": `Bearer ${apiKey}`,
 };
-
 const endpoint = "https://api.start.gg/gql/alpha";
 
-const query = {
-  operationName: "getTournaments",
-  query: `query Sets {
-    player(id: 1000) {
-      id
-      sets(perPage: 5, page: 10) {
-        nodes {
-          id
-          displayScore
-          event {
-            id
-            name
-            tournament {
-              id
-              name
-            }
-          }
-        }
-      }
-    }
-  }`,
-  // variables: {
-  //   perPage: 4,
-  //   coordinates: "33.7454725,-117.86765300000002",
-  //   radius: "50mi",
-  // },
-};
-
 const Home = () => {
+
+  const [location, setLocation] = useState([]);
+
+  function error() {
+    console.log("Unable to retrieve your location");
+  }
+
+  function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const location = [lat, lon];
+    setLocation(location);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
+
   const graphqlTest = async () => {
     try {
       const response = await axios({
@@ -74,8 +63,8 @@ const Home = () => {
           `,
           variables: {
             "perPage": 100,
-            "coordinates": "40.72515026722599, -73.99676899560035",
-            "radius": "5mi"
+            "coordinates": `${location[0]}, ${location[1]}`,
+            "radius": "10mi"
           }
         },
       });
@@ -88,22 +77,7 @@ const Home = () => {
 
   graphqlTest();
 
-  const [location, setLocation] = useState([]);
 
-  function error() {
-    console.log("Unable to retrieve your location");
-  }
-
-  function success(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const location = [lat, lon];
-    setLocation(location);
-  }
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success, error);
-  }, []);
 
   return (
     <main>
